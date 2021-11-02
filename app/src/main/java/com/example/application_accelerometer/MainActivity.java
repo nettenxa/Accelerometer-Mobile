084a8985df3ba797d77a5f5372d0d050b8a433d4
nettenxa <atibodee.mah@dome.tu.ac.th>
2021-11-02 17:56:10 +0700

    ทดสอบ

---
+++ app/src/main/java/com/example/application_accelerometer/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.application_accelerometer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+//public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private  static final  String TAG = "MainActivity";
+
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+
+    TextView xValue, yValue, zValue, MsgTxt;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mRootReference = firebaseDatabase.getReference();
+    private DatabaseReference mChildReference = mRootReference.child("message");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xValue = (TextView) findViewById(R.id.xValue);
+        yValue = (TextView) findViewById(R.id.yValue);
+        zValue = (TextView) findViewById(R.id.zValue);
+
+        MsgTxt = (TextView) findViewById(R.id.msgTxt);
+
+        Log.d(TAG, "onCreate: Intializing Sensor Service ");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registed accelerometer listener");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mChildReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String message = dataSnapshot.getValue(String.class);
+                MsgTxt.setText(message);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Log.d(TAG, "onSensorChanged: X:"+ sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+        xValue.setText("xValue: " +sensorEvent.values[0]);
+        yValue.setText("yValue: " +sensorEvent.values[1]);
+        zValue.setText("zValue: " +sensorEvent.values[2]);
+
+    }
+//Toast.makeText(context: MainActivity.this, text:"Firebase connection Success", Toast.LENGTH_LONG).show();
+
+}
