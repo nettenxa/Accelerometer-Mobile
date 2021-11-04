40ff1982884b6c32d73b2d0986594fb5b6e5338e
nettenxa <atibodee.mah@dome.tu.ac.th>
2021-11-04 15:12:53 +0700

    ss

--- app/src/main/java/com/example/application_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/application_accelerometer/MainActivity.java
@@ -19,8 +19,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+public class MainActivity extends AppCompatActivity {
 //public class MainActivity extends AppCompatActivity implements SensorEventListener {
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private  static final  String TAG = "MainActivity";
 
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Sensor accelerometer;
 
     TextView xValue, yValue, zValue, MsgTxt;
-    TextView tv_lat, tv_lon, tv_altitude, tv_accuract, tv_speed, tv_sensor, tv_updates, tv_address;
-    Switch sw_locationupdate, sw_gps;
 
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference mRootReference = firebaseDatabase.getReference();
@@ -40,29 +38,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        xValue = (TextView) findViewById(R.id.xValue);
-        yValue = (TextView) findViewById(R.id.yValue);
-        zValue = (TextView) findViewById(R.id.zValue);
+//        xValue = (TextView) findViewById(R.id.xValue);
+//        yValue = (TextView) findViewById(R.id.yValue);
+//        zValue = (TextView) findViewById(R.id.zValue);
 
         MsgTxt = (TextView) findViewById(R.id.msgTxt);
 
-        Log.d(TAG, "onCreate: Intializing Sensor Service ");
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        Log.d(TAG, "onCreate: Intializing Sensor Service ");
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//
+//        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+//        Log.d(TAG, "onCreate: Registed accelerometer listener");
 
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        Log.d(TAG, "onCreate: Registed accelerometer listener");
-
-        tv_lat = findViewById(R.id.tv_lat);
-        tv_lon = findViewById(R.id.tv_lon);
-        tv_altitude = findViewById(R.id.tv_altitude);
-        tv_accuract = findViewById(R.id.tv_accuracy);
-        tv_speed = findViewById(R.id.tv_speed);
-        tv_sensor = findViewById(R.id.tv_sensor);
-        tv_updates = findViewById(R.id.tv_updates);
-        tv_address = findViewById(R.id.tv_address);
-        sw_gps = findViewById(R.id.gps);
-        sw_gps = findViewById(R.id.location);
     }
 
     @Override
@@ -83,19 +71,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//    }
 
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        Log.d(TAG, "onSensorChanged: X:"+ sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
-        xValue.setText("xValue: " +sensorEvent.values[0]);
-        yValue.setText("yValue: " +sensorEvent.values[1]);
-        zValue.setText("zValue: " +sensorEvent.values[2]);
-
-    }
+//    @Override
+//    public void onSensorChanged(SensorEvent sensorEvent) {
+//        Log.d(TAG, "onSensorChanged: X:"+ sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
+//        xValue.setText("xValue: " +sensorEvent.values[0]);
+//        yValue.setText("yValue: " +sensorEvent.values[1]);
+//        zValue.setText("zValue: " +sensorEvent.values[2]);
+//
+//    }
 //Toast.makeText(context: MainActivity.this, text:"Firebase connection Success", Toast.LENGTH_LONG).show();
 
 }
